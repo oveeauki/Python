425b466aba89a3656cc4911abcb87afd2d198451
Elias <62937543+oveeauki@users.noreply.github.com>
2020-09-01 09:53:09 +0300

    Update Python Hashing Example.py

--- Python Hashing Example.py
+++ Python Hashing Example.py
@@ -1,10 +1,10 @@
 import hashlib
 import time
-print("This script helps to outline how hashing algorithm works")
-print("You can learn and do many things with hashlib for better documentation visit")
+print("This script helps to outline how hashing algorithms work")
+print("You can learn more and do many things with hashlib for better documentation visit")
 print("https://docs.python.org/3/library/hashlib.html")
 time.sleep(2)
-print("Please enter value you want to encrypt (sha512)")
+print("Please enter value you want to encrypt (sha512 by default)")
 on = input("")
 
 result = on.encode()
